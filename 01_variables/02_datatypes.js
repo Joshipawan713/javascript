a87893d22b9ee0dada14e2c2c86e98ca9a507cc5
Pawan Joshi <149384045+Joshipawan713@users.noreply.github.com>
2025-09-20 18:04:30 +0530

    Add files via upload

---
+++ 01_variables/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict" // treat all js code and newer version
+
+// alert(3+3) // we are using node js not not brower
+
+console.log(3+3) // code readbility should be high
+
+// datatype
+// number => 2 to power 53
+// bigint
+// string => ""
+//  booleen => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+console.log(typeof undefined)
+console.log(typeof null)

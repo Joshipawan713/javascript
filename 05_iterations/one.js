55a66268012d839c3742e801d3c7efcb41cd960d
Pawan Joshi <149384045+Joshipawan713@users.noreply.github.com>
2025-09-30 08:41:02 +0530

    Create one.js

---
+++ 05_iterations/one.js
@@ -0,0 +1,46 @@
+// for loop
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5){
+        // console.log("5 is the best number");
+    }
+    // console.log(element);
+}
+
+for (let i = 1; i <= 10; i++) {
+    const element = i;
+    // console.log(`Outer Loop ${i}`);
+    for (let j = 1; j <= 10; j++) {
+        const element = j;
+        // console.log(`Inner Loop ${i} and Inner Loop ${j}`);
+        // console.log(i + '*' + j + ' = ' + i*j);
+    }
+}
+
+
+// let myarr = ['flash', 'batman', 'superman'];
+// console.log(myarr.length);
+// for (let index = 0; index < myarr.length; index++) {
+//     const element = myarr[index];
+//     console.log(element);
+// }
+
+
+// break and continue
+
+// for (let index = 1; index < 20; index++) {
+//     if (index == 5){
+//         console.log(`Detected 5`);
+//         break;
+//     }
+//     console.log(`Value of i is ${index}`);
+// }
+
+for (let index = 1; index < 20; index++) {
+    if (index == 5){
+        console.log(`Detected 5`);
+        continue;
+    }
+    console.log(`Value of i is ${index}`);
+}

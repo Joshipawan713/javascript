85fbd7554ca2689fa5c97c3269f1ef754ef96e91
Pawan Joshi <149384045+Joshipawan713@users.noreply.github.com>
2025-10-27 16:54:09 +0530

    Create notes.js

---
+++ 10_classes_and_oops/notes.js
@@ -0,0 +1,19 @@
+// javascript and classes
+
+// OOP
+
+// Object - Collection of properties and methods
+
+// parts of OOP - Object literal
+
+// Constructor Function
+// Prototypes
+// Classes
+// Instances (new, this)
+
+
+// 4 pillars - 
+// Abstraction
+// Encapsulation
+// Inheritance
+// Polymorphism
